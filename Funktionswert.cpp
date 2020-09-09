3963af812d29049c1f36f0c4a3687fd2bb2e8f4a
schafhierte204 <hafejens@gmail.com>
2020-09-09 08:30:13 +0200

    Ausgabe wenn Nullstelle

--- Funktionswert.cpp
+++ Funktionswert.cpp
@@ -5,7 +5,21 @@ using namespace std;
 main()
 {
 	double x;
-	cout<<"Bitte Funktionswert Eingben :";
+    double erg;
+	cout<<"Bitte Funktionswert Eingben :"<<endl;
 	cin>> x;
-	cout<<"f(x) = 3*"<<x<<"+7 = "<<3*x+7;
+	erg = 3*x+7;
+    if ( erg <0)
+    {
+        cout<<"Der Wert ist negativ"<<endl;
+    }
+    else if ( erg == 0)
+    {
+        cout<<"Das ist die Nullstelle"<<endl;
+    }
+    else 
+    {
+        cout<<"Der Wert ist positiv"<<endl;
+    }
+    cout <<erg;
 }
