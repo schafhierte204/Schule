edf9b77c09465ba3bda6c08a0f7702ad08659a28
schafhierte204 <hafejens@gmail.com>
2020-09-22 08:52:11 +0200

    bankautomat und parkuhr

---
+++ Bankautomat.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+main()
+{
+    int kstd=500;
+    int dispo=10;
+    int ausbz;
+    char wahl;
+    cout<<"Hallo, Moechten sie ihren kontostand sehen?"<<endl<<"Dann geben sie K ein :";
+    cin>>wahl;
+    if(wahl=='K'||wahl=='k')
+    {
+        cout<<"Sie haben "<<kstd<<" Euro auf dem Konto";
+    }
+        else
+        {
+            cout<<"Bitte Geben Sie den Betrag ein den sie Abbuchen Wollen ";
+            cin>>ausbz;
+            if(ausbz>kstd+kstd/100*dispo)
+            {
+                cout<<"Sie Ueberziehen ihr konto zufiel"<<endl;
+            }
+                else if(ausbz>kstd)
+                {
+                    cout<<"Ihr Guthaben betraegt nur "<<kstd<<" Euro"<<endl;
+                    cout<<"Wollen sie ihr Konto uberziehen (J/N)? ";
+                    cin>>wahl;
+                    if(wahl=='J'||wahl=='j') 
+                    {
+                        kstd=kstd-ausbz;
+                    }
+                }
+                else
+                {
+                    cout<<"sie erhalten Hiermit den gewuenschten Betrag von "<<ausbz<<" Euro"<<endl;
+                    kstd=kstd-ausbz;
+                }
+
+            cout<<"Ihr Konstostand betraegt "<<kstd<<" Euro,"<<endl<<"Bis bald";
+        }
+
+}
---
+++ Parkuhr.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+main()
+{
+    int einstunde,ausstunde;
+    int einminute,ausminute;
+    double minuten;
+    double preis = 2.50;
+    cout<<"Bitte geben sie die Einfahrtszeit ein in stunde und minute";
+    cin>>einstunde;
+    cin>>einminute;
+    if(einstunde>=24||einstunde<0||einminute<0||einminute>=60)
+    {
+        cout<<"Bitte Geben sie eine Uhrzeit ein";
+    }
+    else
+    {
+        cout<<"Bitte geben sie die Ausfahrtszeit ein in stunde und minute";
+        cin>>ausstunde;
+        cin>>ausminute;
+        if(ausstunde>=24||ausstunde<0||ausminute<0||ausminute>=60)
+        {
+            cout<<"Bitte Geben sie eine Uhrzeit ein";
+        }
+        else if (ausstunde<einstunde)
+        {
+            cout<<"Sie haben die Zahlen verwechselt";
+        }
+        else
+        {
+            minuten=(ausstunde-einstunde)*60-einminute+ausminute;
+            cout<<"Sie muessen fuer "<<minuten<<" minuten "<<minuten/60*preis<<" Euro Zahlen";
+        }
+    }
+}
