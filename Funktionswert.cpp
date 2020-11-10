03f32c4bd1f764a25a3c27bcd7829dd62541ca51
schafhierte204 <hafejens@gmail.com>
2020-11-10 07:46:17 +0100

    Schleifen hinzugefügt

--- Funktionswert.cpp
+++ Funktionswert.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
-#include <conio.h>
 using namespace std;
 
 main()
 {
-	double x;
-    double erg;
-	cout<<"Bitte Funktionswert Eingben :"<<endl;
-	cin>> x;
-	erg = 3*x+7;
-    if ( erg <0)
+    double start,stop,schritt;
+    double a,b;
+    COUt<<"Bitte Geben sie a Ein."<<endl;
+    cin>>a;
+    cout>>"Bitte Geben sie b Ein."<<endl;
+    cin>>b;
+    cout<<"Bitte StartWert Eingben :"<<endl;
+    cin>> start;
+    cout<<"Bitte Endwert Eingeben :"<<endl;
+    cin>> stop;
+    cout<<"Bitte Shrittweite Eingeben :"<<endl;
+    cin>> schritt;
+    if(start<stop&&schritt>0)
     {
-        cout<<"Der Wert ist negativ"<<endl;
-    }
-        else if ( erg == 0)
+        for(int i=start;i<stop;i=i+schritt)
         {
-            cout<<"Das ist die Nullstelle"<<endl;
+            cout<<i<<"="<<i*3+5<<endl;
         }
-            else 
-            {
-                cout<<"Der Wert ist positiv"<<endl;
-            }
-    cout <<erg;
+    }
+    else
+    {
+        cout<<"Bitte Uberprufen sie ihre Eingaben"<<endl;
+    }
 }
---
+++ Schleifen/Output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int reien;
+    cout<<"Wiefiele Sternchen reien mochten sie haben"<<endl;
+    cin>> reien;
+    if (reien<0)
+    {
+        cout<<"Bitte Geben sie eine Positive Zahl ein"<<endl;
+    }
+    else
+    {
+        for (int i=0;i<reien;i++)
+        {
+            cout<<"Reihe "<<i+1<<" *****"<<endl;
+        }
+    }
+}
