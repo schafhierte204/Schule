232c36c4ffa171545cad24da267a038c9877817d
schafhierte204 <hafejens@gmail.com>
2020-11-26 11:45:00 +0100

    Addieren hinzufügen

---
+++ Addieren.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+main()
+{
+	int zahl1;
+	int zahl2;
+	cout<<"Bitte Erste Zahl eingeben :";
+	cin>>zahl1;
+	cout<<"Bite Zweite Zahl eingeben :";
+	cin>>zahl2;
+	cout<<zahl1<<"+"<<zahl2<<"="<<zahl1+zahl2;
+}
