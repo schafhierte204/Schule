03a2f79fd249d0127d7add52eb7a0af25fde5c83
schafhierte204 <hafejens@gmail.com>
2021-02-22 11:27:33 +0100

    cleanup

--- do-while_Schleifen/eine_1.cpp
+++ do-while_Schleifen/eine_1.cpp
@@ -8,6 +8,10 @@ int main()
     {
         cout<<"Bitte Geben sie eine Zahl ein."<<endl;
         cin >>zahl;
+        if(zahl!='1')
+        {
+            cout<<"Das war eine "<<zahl<<" Versuchen Sie es noch Einmal!"<<endl;
+        }
     }
     while(zahl!='1');
     cout <<"Danke, dass sie eine 1 eingegeben haben";
