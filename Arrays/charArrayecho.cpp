5fc230986a10b3de32598dffd727cd56d818b69f
schafhierte204 <hafejens@gmail.com>
2021-04-12 12:26:52 +0200

    Echo Char Array

---
+++ Arrays/charArrayecho.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char wort[20]={0};
+    char wort2[20]={0};
+    cout<<"Bitte Geben sie Ihren Namen ein"<<endl;
+    cin>>wort;
+    cin>>wort2;
+    cout<<wort<<" "<<wort2;
+}
