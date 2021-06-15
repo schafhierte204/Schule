420bd0ca19a3ba1c8a02b9a919d2c63a33fd3e46
schafhierte204 <hafejens@gmail.com>
2021-06-15 13:47:51 +0200

    Vergleich in funktion auslagern

--- Funktionen/Huetchenspiel.cpp
+++ Funktionen/Huetchenspiel.cpp
@@ -4,53 +4,58 @@ using namespace std;
 
 int zufall()
 {
-    srand(time(NULL));    
-    return rand()%3+1;
+    return rand() % 3 + 1;
 }
 
 int waehlen()
 {
     int wahl;
     bool fehler = false;
-    cout<<"Bitte wählen sie eins der drei hütchen(1-3)"<<endl;
+    cout << "Bitte wählen sie eins der drei hütchen(1-3)" << endl;
     do
     {
         fehler = false;
-        if(!(cin>>wahl))
+        if (!(cin >> wahl))
         {
             cin.clear();
             cin.ignore();
-            cout<<"Bitte geben sie eine Zahl ein"<<endl;
+            cout << "Bitte geben sie eine Zahl ein" << endl;
             fehler = true;
         }
-        else if (wahl<1||wahl>3)
+        else if (wahl < 1 || wahl > 3)
         {
-            cout<<"Bitte geben sie eine Zahl zwischen 0 und 4 ein"<<endl;
+            cout << "Bitte geben sie eine Zahl zwischen 0 und 4 ein" << endl;
             fehler = true;
         }
     } while (fehler);
     return wahl;
 }
+
+void vergleich()
+{
+    if (zufall() == waehlen())
+    {
+        cout << "Sie lagen richtig" << endl;
+    }
+    else
+    {
+        cout << "Sie haben falsch getippt" << endl;
+    }
+}
 int main()
 {
+    srand(time(NULL));
     char wahl;
-    cout<<"Hallo Wollen sie das Spiel Starten?"<<endl;
-    cin>> wahl;
-    if(wahl=='j')
+    cout << "Hallo Wollen sie das Spiel Starten?" << endl;
+    cin >> wahl;
+    if (wahl == 'j')
     {
         do
         {
-        
-        if(zufall()==waehlen())
-        {
-            cout<<"Sie lagen richtig"<<endl;
-        }
-        else
-        {
-            cout<<"Sie haben falsch getippt"<<endl;
-        }
-        cout<<"Wollen sie Nochmal Spielen(j/n)"<<endl;
-        cin>>wahl;
-        }while(wahl=='j');
+            vergleich();
+
+            cout << "Wollen sie Nochmal Spielen(j/n)" << endl;
+            cin >> wahl;
+        } while (wahl == 'j');
     }
 }
